3cdd199c8b81aac5c7b9e229d0cd076892407f99
panduhzz <christopherchannn@gmail.com>
2024-03-18 15:57:51 -0700

    wrote some terraform for frontend

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "3.96.0"
     }
   }
 }
@@ -26,6 +26,7 @@ resource "azurerm_storage_account" "front_end" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   
+  #Defines that the storage account will be a static website which automatically creates a '$web' blob
   static_website {
     index_document = "index.html"
     error_404_document = "error.html"
@@ -58,4 +59,42 @@ resource "azurerm_storage_blob" "script_js" {
   type                   = "Block"
   source                 = "path/to/your/script.js"
   content_type           = "application/javascript"
+}
+
+#CDN and DNS Configuration
+resource "azurerm_dns_zone" "panduhz_dns_zone" {
+  name = "panduhz.com"
+  resource_group_name = azurerm_resource_group.web_rg.name
+}
+#configuring CNAME for dns zone
+resource "azurerm_dns_cname_record" "azure_resource" {
+  name                = "target"
+  zone_name           = azurerm_dns_zone.panduhz_dns_zone.name
+  resource_group_name = azurerm_resource_group.web_rg.name
+  ttl                 = 300
+  target_resource_id              = "${azurerm_storage_account.front_end.primary_web_host}"
+}
+#alias reference CNAME
+resource "azurerm_dns_cname_record" "target" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.panduhz_dns_zone.name
+  resource_group_name = azurerm_resource_group.web_rg.name
+  ttl                 = 300
+  record  = "panduhz.com"
+}
+resource "azurerm_cdn_frontdoor_profile" "panduhz_door" {
+  name = "panduhz_profile"
+  resource_group_name = azurerm_resource_group.web_rg.name
+  sku_name = "Standard_AzureFrontDoor"
+}
+resource "azurerm_cdn_frontdoor_custom_domain" "fd_custom" {
+  name = "customdomain_frontdoor"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.panduhz_door.id
+  dns_zone_id = azurerm_dns_zone.panduhz_dns_zone.id
+  host_name = "panduhz.com"
+
+  tls {
+    certificate_type = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
 }
