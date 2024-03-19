5d70852cf1eebaebe15237fb09bf6db3d109aaf3
Christopher Chan <christopherchannn@gmail.com>
2024-03-19 16:09:43 -0700

    configured the correct CNAME records, still need to figure out custom domain for endpoint

--- main.tf
+++ main.tf
@@ -15,40 +15,57 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "web_rg" {
-  name = "panduhz_front_end_rg"
+  name     = "panduhz_front_end_rg"
   location = "westus2"
 }
 
 resource "azurerm_storage_account" "front_end" {
-  name                     = "panduhz_front_storageacct"
-  resource_group_name      = azurerm_resource_group.web_rg.name
-  location                 = azurerm_resource_group.web_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  
+  name                            = "panduhzstorage"
+  resource_group_name             = azurerm_resource_group.web_rg.name
+  location                        = azurerm_resource_group.web_rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = true
+
   #Defines that the storage account will be a static website which automatically creates a '$web' blob
   static_website {
-    index_document = "index.html"
+    index_document     = "index.html"
     error_404_document = "error.html"
   }
 }
-
+resource "azurerm_storage_container" "webcontainer" {
+  name                  = "$web"
+  storage_account_name  = azurerm_storage_account.front_end.name
+  container_access_type = "container"
+}
 resource "azurerm_storage_blob" "index_html" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.front_end.name
-  storage_container_name = "$web"  # Use the $web container for static website files
+  storage_container_name = "$web" # Use the $web container for static website files
   type                   = "Block"
-  source                 = "path/to/your/index.html"
-  content_type = "text/html"
+  source                 = "/Users/christopherchan/CloudResumeChallenge/index.html"
+  content_type           = "text/html"
+  # Other configurations like content_type may be necessary depending on your files
+}
+
+resource "azurerm_storage_blob" "syle_css" {
+  name                   = "style.css"
+  storage_account_name   = azurerm_storage_account.front_end.name
+  storage_container_name = "$web" # Use the $web container for static website files
+  type                   = "Block"
+  source                 = "/Users/christopherchan/CloudResumeChallenge/style.css"
+  content_type           = "style/css"
   # Other configurations like content_type may be necessary depending on your files
 }
 
 resource "azurerm_storage_blob" "error_html" {
   name                   = "error.html"
   storage_account_name   = azurerm_storage_account.front_end.name
-  storage_container_name = "$web"  # Use the $web container for static website files
+  storage_container_name = "$web" # Use the $web container for static website files
   type                   = "Block"
-  content_type = "text/css"
+  source                 = "/Users/christopherchan/CloudResumeChallenge/error.html"
+  content_type           = "text/html"
   # Other configurations like content_type may be necessary depending on your files
 }
 
@@ -57,44 +74,61 @@ resource "azurerm_storage_blob" "script_js" {
   storage_account_name   = azurerm_storage_account.front_end.name
   storage_container_name = "$web"
   type                   = "Block"
-  source                 = "path/to/your/script.js"
+  source                 = "/Users/christopherchan/CloudResumeChallenge/script.js"
   content_type           = "application/javascript"
 }
 
 #CDN and DNS Configuration
 resource "azurerm_dns_zone" "panduhz_dns_zone" {
-  name = "panduhz.com"
+  name                = "panduhz.com"
   resource_group_name = azurerm_resource_group.web_rg.name
 }
 #configuring CNAME for dns zone
 resource "azurerm_dns_cname_record" "azure_resource" {
-  name                = "target"
+  name                = "cdnverify.www"
   zone_name           = azurerm_dns_zone.panduhz_dns_zone.name
   resource_group_name = azurerm_resource_group.web_rg.name
   ttl                 = 300
-  target_resource_id              = "${azurerm_storage_account.front_end.primary_web_host}"
+  record              = "cdnverify.${azurerm_cdn_endpoint.example.name}.azureedge.net"
 }
 #alias reference CNAME
 resource "azurerm_dns_cname_record" "target" {
+  depends_on          = [azurerm_cdn_endpoint.example]
   name                = "www"
   zone_name           = azurerm_dns_zone.panduhz_dns_zone.name
   resource_group_name = azurerm_resource_group.web_rg.name
   ttl                 = 300
-  record  = "panduhz.com"
+  target_resource_id =  azurerm_cdn_endpoint.example.id
 }
-resource "azurerm_cdn_frontdoor_profile" "panduhz_door" {
-  name = "panduhz_profile"
+resource "azurerm_cdn_profile" "panduhzprofile" {
+  name                = "panduhz-cdn"
+  location            = "global"
   resource_group_name = azurerm_resource_group.web_rg.name
-  sku_name = "Standard_AzureFrontDoor"
+  sku                 = "Standard_Verizon"
 }
+resource "azurerm_cdn_endpoint" "example" {
+  name                = "panduhz-tftest"
+  profile_name        = azurerm_cdn_profile.panduhzprofile.name
+  location            = "global"
+  resource_group_name = azurerm_resource_group.web_rg.name
+  is_http_allowed     = true
+  is_https_allowed    = true
+  origin {
+    name      = "default-origin"
+    host_name = "${azurerm_storage_account.front_end.name}.blob.core.windows.net"
+  }
+}
+
+/*
 resource "azurerm_cdn_frontdoor_custom_domain" "fd_custom" {
-  name = "customdomain_frontdoor"
+  name                     = "customdomain-frontdoor"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.panduhz_door.id
-  dns_zone_id = azurerm_dns_zone.panduhz_dns_zone.id
-  host_name = "panduhz.com"
+  dns_zone_id              = azurerm_dns_zone.panduhz_dns_zone.id
+  host_name                = "panduhz.com"
 
   tls {
-    certificate_type = "ManagedCertificate"
+    certificate_type    = "ManagedCertificate"
     minimum_tls_version = "TLS12"
   }
 }
+*/
