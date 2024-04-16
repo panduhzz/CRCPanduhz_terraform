9088b093bc92eb365564e2544f09c6cf4aa1aa87
Christopher Chan <christopherchannn@gmail.com>
2024-04-15 20:41:45 -0700

    update js to use GET and POST requests

--- web/script.js
+++ web/script.js
@@ -1,5 +1,57 @@
+//POST request
+fetch("https://backend-function-app.azurewebsites.net/api/updateDB?", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+    // Additional headers can be set here if required
+  },
+  body: JSON.stringify({
+  })
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
+.then(data => {
+  console.log(data);
+  console.log("message: " + data.message);
+  console.log("Updated count: " + data.updateCount);
+})
+.catch(error => {
+  console.error('There has been a problem with your fetch operation:', error);
+})
+.finally(() => {
+  console.log("Entity has been updated");
+});
+
+//GET request
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://backend-function-app.azurewebsites.net/api/http_trigger?code=qdX66yWpXc1dpqLybHgjJVOuk1aE9I-c0Eeoum2jwQETAzFuSEky_w==')
+fetch("https://backend-function-app.azurewebsites.net/api/readDB?")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    console.log("message" + data.message);
+    console.log("count" + data.count);
+    document.querySelector('.visitor-counter').textContent = `Visitor Count: ${data.count}`;
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  })
+  .finally(() => {
+    console.log("DB read");
+  });
+})
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://backend-function-app.azurewebsites.net/api/http_trigger?code=qdX66yWpXc1dpqLybHgjJVOuk1aE9I-c0Eeoum2jwQETAzFuSEky_w==', POST)
   .then(response => {
     if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -15,3 +67,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     console.log("The page has fully loaded");
 });
+*/
